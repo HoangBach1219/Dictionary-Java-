0bb18f368277ed4f1dda020ad93b08061c9c09cb
Bach Hoang Huu <19020093@vnu.edu.vn>
2020-10-04 02:18:25 +0700

    Basic Dictionary - Without Graphic UI Design

---
+++ src/main/java/com/bachhh/dictionarybasesetup/Dictionary.java
@@ -0,0 +1,31 @@
+package com.bachhh.dictionarybasesetup;
+
+import java.util.ArrayList;
+
+public class Dictionary {
+    Word word =new Word();
+    public ArrayList<Word> words =  new ArrayList<Word>();
+
+    public Word getWord() {
+        return word;
+    }
+
+    public void setWord(Word word) {
+        this.word = word;
+    }
+
+    public ArrayList<Word> getWords() {
+        return words;
+    }
+
+    public void setWords(ArrayList<Word> words) {
+        this.words = words;
+    }
+
+
+
+
+    private int wordsCount = 0;
+
+}
+
